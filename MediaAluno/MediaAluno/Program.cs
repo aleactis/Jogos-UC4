9590e15512d1dc15711f5102985616f50fbefaa0
Alexandre Dionizio <alexandre.dio@gmail.com>
2018-12-04 16:17:49 -0200

    Primeiro commit

---
+++ MediaAluno/MediaAluno/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            string nome;
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite a nota do aluno: ");
+            nota = int.Parse(Console.ReadLine());
+
+            if (nota >= 7)
+            {
+                Console.WriteLine("Aluno Aprovado");
+
+            }
+            else if (nota >= 5)
+            {
+                Console.WriteLine("Aluno em Recuperação");
+            }
+            else
+            {
+                Console.WriteLine("Aluno Reprovado");
+            }
+        }
+    }
+}

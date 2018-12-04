2e90d3d0d0c3c99fedbd636880e31771bd3b7004
Alexandre Dionizio <alexandre.dio@gmail.com>
2018-12-04 16:14:02 -0200

    Primeiro commit

---
+++ ComandoSwitch/ComandoSwitch/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComandoSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diaSemana;
+            string nome;
+            Console.Write("Digite um dia da semana de 1 a 7: ");
+            diaSemana = int.Parse(Console.ReadLine());
+
+            switch (diaSemana)
+            {
+                case 1:
+                    Console.WriteLine("Domingo");
+                    break;
+                case 2:
+                    Console.WriteLine("Segunda");
+                    break;
+                case 3:
+                    Console.WriteLine("Terça");
+                    break;
+                case 4:
+                    Console.WriteLine("Quarta");
+                    break;
+                case 5:
+                    Console.WriteLine("Quinta");
+                    break;
+                case 6:
+                    Console.WriteLine("Sexta");
+                    break;
+                case 7:
+                    Console.WriteLine("Sábado");
+                    break;
+                default:
+                    Console.WriteLine("Dia inválido");
+                    break;
+                
+                /*
+                case 1:
+                case 7:
+                    Console.WriteLine("Final de Semana");
+                    break;
+                case 2:
+                    Console.WriteLine("Segunda");
+                    break;
+                case 3:
+                    Console.WriteLine("Terça");
+                    break;
+                case 4:
+                    Console.WriteLine("Quarta");
+                    break;
+                case 5:
+                    Console.WriteLine("Quinta");
+                    break;
+                case 6:
+                    Console.WriteLine("Sexta");
+                    break;
+                default:
+                    Console.WriteLine("Dia inválido");
+                    break;
+                */
+            }
+        }
+    }
+}

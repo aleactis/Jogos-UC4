8646377b05d7ccc72fb06f6045d91f99c546d59f
Alexandre Dionizio <alexandre.dio@gmail.com>
2018-12-04 16:19:06 -0200

    Primeiro Commit

---
+++ VerificarNumeros/VerificarNumeros/MediaAluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificarNumeros
+{
+    class MediaAluno
+    {
+        string nome;
+        int n1, n2, n3;
+
+        Console.Write("Digite o nome do aluno: ");
+    }
+}
---
+++ VerificarNumeros/VerificarNumeros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificarNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("Digite um número: ");
+            x = int.Parse(Console.ReadLine());
+            Console.ReadKey();
+
+            Console.WriteLine("Digite outro número: ");
+            y = int.Parse(Console.ReadLine());
+
+            if (x == y)
+            {
+                Console.WriteLine("Os números são iguais");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Os números são diferentes");
+                Console.ReadKey();
+            }
+        }
+    }
+}

45298f3448fd3df70db3164894d42138f86a8fda
Alexandre Dionizio <alexandre.dio@gmail.com>
2018-12-04 16:16:30 -0200

    Primeiro commit

---
+++ ParImpar/ParImpar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Digite um número: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("O número digitado é par");
+            }
+            else
+            {
+                Console.WriteLine("O número digitado é ímpar");
+            }
+        }
+    }
+}
